6c7b803917f41828e6111df526af914fdba60e52
zeynepkryll <zeynep.karayel52@gmail.com>
2025-01-25 18:00:28 +0300

    Add files via upload

---
+++ 1.haftaorta.py
@@ -0,0 +1,12 @@
+sayi1 = input("1. Sayıyı Giriniz:")
+sayi2 = input("2. Sayıyı Giriniz:")
+sayi1 = float(sayi1)
+sayi2 = float(sayi2)
+top = sayi1 + sayi2
+cık = sayi1 - sayi2
+car = sayi1 * sayi2
+böl = sayi1 / sayi2
+print(f"Toplam: {top}")
+print(f"Fark: {cık} ")
+print(f"Çarpım: {car}")
+print(f"Bölüm: {böl}")
