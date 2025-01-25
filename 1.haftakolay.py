e4c66d3d4d1d868cea4429c19f25db4408ca8698
zeynepkryll <zeynep.karayel52@gmail.com>
2025-01-25 17:47:46 +0300

    Add files via upload
    elimden geleni yaptım oldu mu bilmiyorum

---
+++ 1.haftakolay.py
@@ -0,0 +1,7 @@
+ad = input("Adınızı Giriniz:")
+soyad = input("Soyadınızı Giriniz:")
+no = input("Okul Numaranızı Giriniz:")
+ders = input("Ders Adını Giriniz:")
+ort = input("Ortalamanızı Giriniz:")
+
+print(f"Okulumuz {no} nolu öğrencisi {ad} {soyad}, bu dönem sonunda {ders} dersinden {ort} ortalama elde etmiştir.")
